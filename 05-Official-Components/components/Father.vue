0d315ce04b18fcfd9788ad12e6ba58c0bd1cf389
lesenelir <miaomiaobabyzy@gmail.com>
2022-01-22 13:38:49 +0800

    update

---
+++ 05-Official-Components/components/Father.vue
@@ -0,0 +1,13 @@
+<template>
+$END$
+</template>
+
+<script>
+export default {
+name: "Father"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ 05-Official-Components/components/Son.vue
@@ -0,0 +1,13 @@
+<template>
+$END$
+</template>
+
+<script>
+export default {
+name: "Son"
+}
+</script>
+
+<style scoped>
+
+</style>
