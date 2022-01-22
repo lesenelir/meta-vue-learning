09017705112e887611bbd65e871c9e0f1d34e22b
lesenelir <miaomiaobabyzy@gmail.com>
2022-01-22 13:39:36 +0800

    update

--- 05-Official-Components/components/Father.vue
+++ 05-Official-Components/components/Father.vue
@@ -1,10 +1,27 @@
 <template>
-$END$
+  <div class="father-container">
+    <h3>父级组件</h3>
+    <Son :son-info="userinfo" :son-message="message"></Son>
+  </div>
 </template>
 
 <script>
+// 通过模块化系统局部注册组件
+import Son from "./Son"
 export default {
-name: "Father"
+  name: "Father",
+  data() {
+    return {
+      message: "Father's message",
+      userinfo: {
+        name: "lesenelir",
+        age: 18
+      }
+    }
+  },
+  components: {
+    Son
+  }
 }
 </script>
 
--- 05-Official-Components/components/Son.vue
+++ 05-Official-Components/components/Son.vue
@@ -1,10 +1,18 @@
 <template>
-$END$
+  <div class="son-container">
+    <h3>子组件</h3>
+    <hr>
+
+    <p>父组件向子组件传递数据通过props</p>
+    <p> {{sonMessage}} , {{sonInfo}}</p>
+
+  </div>
 </template>
 
 <script>
 export default {
-name: "Son"
+  name: "Son",
+  props: ["sonMessage",  "sonInfo"]
 }
 </script>
 
